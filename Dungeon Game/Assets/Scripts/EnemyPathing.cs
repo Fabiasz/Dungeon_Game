eebebea03d6ab53a7ede68fa6734c97900c63006
Filip_Nozkiewicz <filas08@gmail.com>
2019-05-04 00:35:46 +0200

    enemy

---
+++ Dungeon Game/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    List<Transform> wayPoints;
+    [SerializeField]
+    WaveConfig waveConfig;
+    int waypointIndex = 0;
+
+    private void Start()
+    {
+        wayPoints = waveConfig.GetwayPoints();
+        transform.position = wayPoints[waypointIndex].transform.position;
+        
+
+    }
+    private void Update()
+    {
+        makePathing();
+       
+       // Debug.Log(wayPoints.Count);
+    }
+    public void makePathing()
+    {
+        if (waypointIndex < wayPoints.Count - 1)
+        {
+            var targetPos = wayPoints[waypointIndex].transform.position;
+            Debug.Log(targetPos.ToString());
+            Debug.Log("thid" + transform.position.ToString());
+
+            var moveFrame = waveConfig.GetSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, moveFrame);
+            if ((transform.position.x == targetPos.x) && (targetPos.y == transform.position.y))
+            {
+               
+                waypointIndex++;
+            } 
+        }
+    }
+
+}
---
+++ Dungeon Game/Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public float GetSpeed()
+    {
+        return moveSpeed;
+    }
+    public List<Transform> GetwayPoints()
+    {
+        var wayPoints = new List<Transform>();
+        foreach(Transform child in pathPrefab.transform)
+        {
+            wayPoints.Add(child.transform);
+           // Debug.Log(child.ToString());
+        }
+        return wayPoints;
+    } 
+    
+
+}
