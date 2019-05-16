fd5e5f234a88b4d08e3d2b5ec10dea1c7583b093
Filip_Nozkiewicz <filas08@gmail.com>
2019-05-16 11:50:57 +0200

    Added AI for enemy , correction of shooting

---
+++ Dungeon Game/Assets/RayCaster.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RayCaster : MonoBehaviour
+{
+    public int rotSpeed = 200;
+    public float distance = 5;
+    [SerializeField]
+    GameObject enemy;
+    Enemy enemyscript;
+    EnemyFollowing enemyFollowing;
+    public float followTime = 5f;
+    bool following = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.queriesStartInColliders = false;
+         enemyscript = transform.parent.gameObject.GetComponent<Enemy>();
+         enemyFollowing = transform.parent.gameObject.GetComponent<EnemyFollowing>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (following)
+            enemyFollowing.FollowPlayer();
+        transform.Rotate(Vector3.forward * rotSpeed   );
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+            if (hitInfo.collider.tag == "Player")
+            {
+                Debug.Log("Collision");
+
+                following = true;
+                   
+                
+
+                enemyscript.Shooting();
+            }
+            else
+            {
+                ;
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.yellow);
+
+        }
+    }
+}
