fe65cf9df6ddb69c0a1e5ba0d669f7edf4b9f353
Wojtek Szczepanik <wojtek100503@gmail.com>
2019-05-17 10:36:41 +0200

    Improved weapon
    Weapons can shot multiple bullets, costs mana, bug fixes

--- Dungeon Game/Assets/Scripts/Player/Weapon.cs
+++ Dungeon Game/Assets/Scripts/Player/Weapon.cs
@@ -14,17 +14,36 @@ public class Weapon : MonoBehaviour
     private float boltCooldown = 0;
     private bool firePressed = false;
     public GameObject boltPrefab;
+    public int bulletsPerShot = 1;
+    public float widthAngle = 0;
+    private float[] angles;
     public float bulletWeaponDistance = 0.1f;
     public float bulletVelocity = 5;
     public float timeBetweenBullets = 0.2f;
     public float bulletLifeTime = 1.5f;
+    public float manaPerShot = 5;
+    private PlayerStats playerStats;
 
     private void Start()
     {
 
         weaponSprite = gameObject.GetComponent<Transform>();
         player = transform.root.GetComponentInChildren<Transform>();
+        playerStats = player.GetComponent<PlayerStats>();
         //player = gameObject.GetComponent<Transform>();
+
+        angles = new float[bulletsPerShot];
+        if (bulletsPerShot == 1)
+        {
+            angles[0] = 0;
+        }
+        else
+        {
+            for (int i = 0; i < bulletsPerShot; i++)
+            {
+                angles[i] = -widthAngle / 2 + i * widthAngle / (bulletsPerShot - 1);
+            }
+        }
     }
     private void Update()
     {
@@ -39,13 +58,19 @@ public class Weapon : MonoBehaviour
 
     private void Fire()
     {
-        if (firePressed && boltCooldown <= 0)
+        if (firePressed && boltCooldown <= 0 && playerStats.mana >= manaPerShot)
         {
-            GameObject bolt = Instantiate(boltPrefab, transform.position + mouseVector * bulletWeaponDistance, Quaternion.identity);
-            bolt.GetComponent<Rigidbody2D>().velocity = bulletVelocity * mouseVector;
-            bolt.transform.Rotate(0.0f, 0.0f, Mathf.Atan2(mouseVector.y, mouseVector.x) * Mathf.Rad2Deg);
-            Destroy(bolt, bulletLifeTime);
+            for (int i = 0; i < bulletsPerShot; i++)
+            {
+                GameObject bolt = Instantiate(boltPrefab, transform.position + mouseVector * bulletWeaponDistance, Quaternion.identity);
+                //Vector3 direction = mouseVector + new Vector3(Mathf.Cos(Mathf.Deg2Rad * angles[i]), Mathf.Sin(Mathf.Deg2Rad * angles[i]));
+                Vector3 direction = Quaternion.Euler(0, 0, angles[i]) * mouseVector;
+                bolt.GetComponent<Rigidbody2D>().velocity = bulletVelocity * direction;
+                bolt.transform.Rotate(0.0f, 0.0f, angles[i] + Mathf.Atan2(mouseVector.y, mouseVector.x) * Mathf.Rad2Deg);
+                Destroy(bolt, bulletLifeTime);
+            }
             boltCooldown = timeBetweenBullets;
+            playerStats.mana -= manaPerShot;
         }
         boltCooldown -= Time.fixedDeltaTime;
     }
