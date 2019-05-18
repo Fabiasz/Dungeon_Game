498bcc14cf725d6e29c5bf880cb8df1cc3f8445e
Filip_Nozkiewicz <filas08@gmail.com>
2019-05-18 09:15:34 +0200

    no message

--- Dungeon Game/Assets/Scripts/Enemy/EnemyFollowing.cs
+++ Dungeon Game/Assets/Scripts/Enemy/EnemyFollowing.cs
@@ -8,32 +8,48 @@ public class EnemyFollowing : MonoBehaviour
     [SerializeField] float stopDist = 5f;
     [SerializeField] float retreatDist = 2f;
     private Transform player;
+    Animator animator;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
+        animator = transform.gameObject.GetComponent<Animator>();
     }
 
     void Update()
     {
-       // FollowPlayer();
+        // FollowPlayer();
     }
 
-    public void FollowPlayer()
+    public void FollowPlayer(Vector2 lastPos , bool activate_last = false)
     {
-        if (Vector2.Distance(transform.position, player.position) > stopDist)
+        if (!activate_last)
         {
+            if (Vector2.Distance(transform.position, player.position) > stopDist)
+            {
 
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                animator.SetBool("PlayerSeen", true);
+            }
+            else if (Vector2.Distance(transform.position, player.position) < stopDist && Vector2.Distance(transform.position, player.position) > retreatDist)
+            {
+                transform.position = this.transform.position;
+                animator.SetBool("PlayerSeen", false);
+            }
+            else if (Vector2.Distance(transform.position, player.position) < retreatDist)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+                animator.SetBool("PlayerSeen", true);
+            }
         }
-        else if (Vector2.Distance(transform.position, player.position) < stopDist && Vector2.Distance(transform.position, player.position) > retreatDist)
+        else
         {
-            transform.position = this.transform.position;
-        }
-        else if (Vector2.Distance(transform.position, player.position) < retreatDist)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, lastPos, speed * Time.deltaTime);
+            animator.SetBool("PlayerSeen", true);
+            if(transform.position.x == lastPos.x && transform.position.y == lastPos.y)
+            {
+                animator.SetBool("PlayerSeen", false);
+            }
         }
     }
 }
--- Dungeon Game/Assets/Scripts/Enemy/RayCaster.cs
+++ Dungeon Game/Assets/Scripts/Enemy/RayCaster.cs
@@ -6,17 +6,22 @@ using UnityEngine;
 public class RayCaster : MonoBehaviour
 {
     public int rotSpeed = 200;
-    public float distance = 5;
+    public float distance = 20;
     [SerializeField]
     GameObject enemy;
     Enemy enemyscript;
     EnemyFollowing enemyFollowing;
     public float followTime = 5f;
     bool following = false;
+    public Animator animator;
+    private float timeBeetweenSeeingPlayer = 2f;
+    Vector2 lastPlayerSeenPos;
+    private bool isOnPlace = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         Physics2D.queriesStartInColliders = false;
          enemyscript = transform.parent.gameObject.GetComponent<Enemy>();
          enemyFollowing = transform.parent.gameObject.GetComponent<EnemyFollowing>();
@@ -26,8 +31,15 @@ public class RayCaster : MonoBehaviour
     void Update()
     {
         if (following)
-            enemyFollowing.FollowPlayer();
-        transform.Rotate(Vector3.forward * rotSpeed   );
+        {
+            enemyFollowing.FollowPlayer(new Vector2(0, 0), false);
+            transform.Rotate(Vector3.forward * rotSpeed);
+
+        }
+        else {
+            enemyFollowing.FollowPlayer(lastPlayerSeenPos, true);
+        }
+
 
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
         if (hitInfo.collider != null)
@@ -35,17 +47,29 @@ public class RayCaster : MonoBehaviour
             Debug.DrawLine(transform.position, hitInfo.point, Color.red);
             if (hitInfo.collider.tag == "Player")
             {
+                
                 Debug.Log("Collision");
 
                 following = true;
-                   
                 
-
                 enemyscript.Shooting();
+                
+                timeBeetweenSeeingPlayer = 2f;
+                lastPlayerSeenPos = hitInfo.collider.transform.position;
             }
             else
             {
-                ;
+                timeBeetweenSeeingPlayer -= Time.deltaTime;
+                if(timeBeetweenSeeingPlayer <= 0)
+                {
+                    following = false;
+                    
+                    /*if (!isOnPlace)
+                    {
+                        enemyFollowing.FollowPlayer(lastPlayerSeenPos, true);
+                    }*/
+
+                }
             }
         }
         else
