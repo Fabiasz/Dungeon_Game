c325cfa18dcc176dc6de0e840de16e467d0662ce
Wojtek Szczepanik <wojtek100503@gmail.com>
2019-05-25 12:53:23 +0200

    Health and Mana Bars

---
+++ Dungeon Game/Assets/HealthBarScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    private PlayerStats playerStats;
+    private Image healthBar;
+    void Start()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        healthBar = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        healthBar.fillAmount = (float)playerStats.health / (float) playerStats.maxHealth;
+    }
+}
---
+++ Dungeon Game/Assets/ManaBarScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBarScript : MonoBehaviour
+{
+    private PlayerStats playerStats;
+    private Image manaBar;
+    void Start()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        manaBar = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        manaBar.fillAmount = (float)playerStats.mana / (float)playerStats.maxMana;
+    }
+}
