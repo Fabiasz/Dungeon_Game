cd62f73506f6a0b4031ec16dab164b6265a608cb
Fabiasz <fabiasz379@gmail.com>
2019-05-26 22:39:07 +0200

    health bars pt2

--- Dungeon Game/Assets/GameRules.cs
+++ Dungeon Game/Assets/GameRules.cs
@@ -6,10 +6,13 @@ public class GameRules : MonoBehaviour
 {
     public int NPCS=0;
     public GameObject  portalObject;
+    public PlayerStats playerStats;
+    
     void Update()
     {
         EnemyCounter();
          VictoryLoading();
+        LosingLoading();
     }
 
    void VictoryLoading()
@@ -23,7 +26,17 @@ public class GameRules : MonoBehaviour
             NPCS = 1;
         }
     }
+    void LosingLoading()
+    {
+        
+        //if (playerStats.health == 0)
+       // {
 
+        //    UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+            
+            
+       // }
+    }
     private  void EnemyCounter()
     {
       GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
